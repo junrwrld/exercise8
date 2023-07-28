d088f3fbd1f477097d4a8bbe07b28224601e391b
João Junior <batistasousajoao@gmail.com>
2023-07-28 19:06:58 -0300

    Adicionar arquivos de projeto.

---
+++ exercise8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise8
+{
+    public class _exercise8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor:");
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
